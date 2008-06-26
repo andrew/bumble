a263a261089bee4ac7da055399abb0f2215e65cf
Andrew Nesbitt <andrewnez@gmail.com>
2008-06-26 08:10:53 +0100

    beginings of a ui

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,1 +1,8 @@
-
+$(document).ready(function(){
+  $("#new-post-tabs > ul").tabs();
+  $(".extra-fields").hide();
+  $(".show-extra-fields").click(function(){
+    $(".extra-fields").toggle();
+    return false;
+  })
+});
