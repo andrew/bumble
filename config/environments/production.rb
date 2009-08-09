8ef772bbd7785768963c30db4fa425379f2c8fc1
Andrew Nesbitt <andrewnez@gmail.com>
2009-08-09 23:50:00 +0100

    use the DOMAIN constant for production mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,4 +27,4 @@ config.action_view.cache_template_loading            = true
 # Enable threaded mode
 # config.threadsafe!
 
-config.action_mailer.default_url_options = { :host => 'youdomain.com' }
+config.action_mailer.default_url_options = { :host => DOMAIN }
