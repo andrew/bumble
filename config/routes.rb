f864d3ada05b94c1d79f53336d85db78bb341293
Andrew Nesbitt <andrewnez@gmail.com>
2010-04-26 22:07:02 +0100

    Removed unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :posts, :collection => {:search => :get}, :has_many => :comments
   map.resources :comments
-  map.resources :password_resets
+  map.resources :password_resets, :except => [:index, :show, :destroy]
   map.resources :users
-  
-  map.login     '/login',                     :controller => "user_sessions", :action => "new", :conditions => {:method => :get}
-  map.logout    '/logout',                    :controller => "user_sessions", :action => "destroy"
-  map.resource  :user_session, :as => 'login'
+
+  map.login     '/login',   :controller => "user_sessions", :action => "new", :conditions => {:method => :get}
+  map.logout    '/logout',  :controller => "user_sessions", :action => "destroy"
+  map.resource  :user_session, :as => 'login', :only => :create
 
   map.activate  '/activate/:activation_code', :controller => 'users',         :action => 'activate'
 
